3200c3dccc5e5febe93cb5efe081417bff431e6c
Francisco Anderson Bezerra Rodrigues <anders1232@mail.com>
2017-04-16 20:15:08 -0300

    Modificado a interface da câmera para dar suporte para o zoom

--- Engine/include/Camera.h
+++ Engine/include/Camera.h
@@ -4,17 +4,28 @@
 #include "Gameobject.h"
 #include "Vec2.h"
 
+#define DEFAULT_MIN_ZOOM (0.3)
+#define DEFAULT_MAX_ZOOM (5.0)
+
 class Camera
 {
 	public:
 		static void Follow(GameObject* newFocus);
 		static void Unfollow(void);
-		 static void Update(float dt);
-		 static Vec2 pos;
-		 static Vec2 speed;
+		static void Update(float dt);
+		static Vec2 pos;
+		static Vec2 speed;
+		static void ForceZoom(float newZoom);
+		static void SetZoomnable(bool zoomnable);
+		static void ChangeZoom(float deltaZoom);
+		static void SetZoomLimits(float minZoom=0, float maxZoom=0);// set to 0 is to set to default
+		static float GetZoom(void);
 	private:
 		Camera();
 		static GameObject* focus;
+		static float currentZoom;
+		static float minZoom;
+		static float maxZoom;
 };
 
 #endif // CAMERA_H
