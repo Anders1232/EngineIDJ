81c354e072ae33cc718b11c013425699db619b2d
Francisco Anderson Bezerra Rodrigues <anders1232@mail.com>
2017-04-28 22:14:30 -0300

    Atualizado a documentação, para conter a documentação do novo atributo do Text.

--- docs/search/all_5.js
+++ docs/search/all_5.js
@@ -3,6 +3,7 @@ var searchData=
   ['focus',['focus',['../classCamera.html#a266c767efbe18088d7f79880f7e98998',1,'Camera']]],
   ['follow',['Follow',['../classCamera.html#a4290d7e4815d2e0726d88b743d44e8df',1,'Camera']]],
   ['font',['font',['../classText.html#a7c89817e7e1d08584cf1b51106ce5ba4',1,'Text']]],
+  ['fontfile',['fontFile',['../classText.html#aaae1164cbb6c2ce3f13e07973c74ed9a',1,'Text']]],
   ['fontsize',['fontSize',['../classText.html#af1b0c4c5d94f1a5338398f37e7b9ebbe',1,'Text']]],
   ['fonttable',['fontTable',['../classResources.html#ab3665e0c725cd7f4dbf65b128f87d0d5',1,'Resources']]],
   ['forcezoom',['ForceZoom',['../classCamera.html#a7dc4b1fd7f5e03ffb25d306fe81ee4c0',1,'Camera']]],
--- docs/search/variables_5.js
+++ docs/search/variables_5.js
@@ -2,6 +2,7 @@ var searchData=
 [
   ['focus',['focus',['../classCamera.html#a266c767efbe18088d7f79880f7e98998',1,'Camera']]],
   ['font',['font',['../classText.html#a7c89817e7e1d08584cf1b51106ce5ba4',1,'Text']]],
+  ['fontfile',['fontFile',['../classText.html#aaae1164cbb6c2ce3f13e07973c74ed9a',1,'Text']]],
   ['fontsize',['fontSize',['../classText.html#af1b0c4c5d94f1a5338398f37e7b9ebbe',1,'Text']]],
   ['fonttable',['fontTable',['../classResources.html#ab3665e0c725cd7f4dbf65b128f87d0d5',1,'Resources']]],
   ['framecount',['frameCount',['../classSprite.html#a8dc8d5c9530bad6113d37fe5e53e4668',1,'Sprite']]],
