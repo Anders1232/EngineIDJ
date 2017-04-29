9d685851b01097148ca389bb6bf18cecbd221647
Francisco Anderson Bezerra Rodrigues <anders1232@mail.com>
2017-04-28 22:06:34 -0300

    Sound.h ocumentado! Modificado Sound.c para garantir que toda vez antes de alterar o estado do Sound o som que esteja tocando seja interrompido.
    A interrupção evita que se perca a informação de onde esse som é tocado. Essa interrupção também é feita caso seja solicitado para o som ser tocado novamente.

--- Engine/include/Sound.h
+++ Engine/include/Sound.h
@@ -22,18 +22,59 @@
 
 using std::string;
 
+/**
+	\brief Classe que modela sons.
+
+	Modela sons e permite operações básicas como: abiri um arquivo de som, tocar som e para som(stop).
+*/
 class Sound
 {
 	public:
+		/**
+			\brief Construtor padrão.
+			\todo Verificar a necessidade da existência desse contrutor.
+
+			Cria uma instância não associada a nenhum som. O uso da instâncis nesse estado pode causar problemas.
+		*/
 		Sound();
+		/**
+			\brief Construtor.
+			\param file Arquivo com som a ser utilizado.
+
+			Cria uma instância associando-a um som. Todas as funcionalidades do arquivo estão disponíveis.
+		*/
 		Sound(string file);
+		/**
+			\brief Toca o som.
+			\param times Número de vezes que a música deve repetir.
+
+			Toca o som, num canal arbritário. Caso essa instância de Sound já esteja com um som sendo tocado, esse som é interrompido.
+			Caso times seja -1, o sem será tocado indefinidamente.
+		*/
 		void Play(int times);
+		/**
+			\brief Interrompe o som.
+
+			Interrompe a eecução do som associado à instância.
+		*/
 		void Stop(void);
+		/**
+			\brief Carrega um som.
+			\param file Arquivo com o som a ser utilizado.
+
+			Associa a instância ao som contido no arquivo. Caso essa instância de Sound já esteja com um som sendo tocado, esse som é interrompido para que depois o novo som seja carregado.
+		*/
 		void Open(string file);
+		/**
+			\brief Verifica se tem um arquivo aberto
+			\param return verdadeiro se essa instância está associada a um som, falso caso contrário.
+
+			Verifica se a instância de Sound está associada ao som de algum arquivo.
+		*/
 		bool IsOpen(void);
 	private:
-		std::shared_ptr<Mix_Chunk> sound;
-		int channel;
+		std::shared_ptr<Mix_Chunk> sound; /**< Ponteiro para o Mix_Chunk com o som.*/
+		int channel;/**< Canal no qual o som está sendo tocado. -1 caso o som não esteja sendo tocado. O valor tem que ser -1 pois esse valor indica na hora da reprodução do som que p sistema pode escolher o canal no qual o som deve ser executado.*/
 };
 
 #endif // SOUND_H
--- Engine/src/Sound.cpp
+++ Engine/src/Sound.cpp
@@ -10,14 +10,23 @@ Sound::Sound(string file): sound(nullptr), channel(-1)
 }
 void Sound::Play(int times)
 {
+	if(-1 != channel)
+	{
+		Stop();
+	}
 	channel= Mix_PlayChannel(-1, sound.get(), times-1);
 }
 void Sound::Stop(void)
 {
 	Mix_HaltChannel(channel);
+	channel= -1;
 }
 void Sound::Open(string file)
 {
+	if(-1 != channel)
+	{
+		Stop();
+	}
 	sound= Resources::GetSound(file);
 }
 bool Sound::IsOpen(void)
