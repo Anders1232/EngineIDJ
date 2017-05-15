2e5f519008d49d6050e4b7ed85db711e0e52addd
Gabriel Barbosa <gabrielfbbarbosa@gmail.com>
2017-05-15 00:13:51 -0300

    Temp fix para error para facilitar isolar o erro

--- Engine/include/Error.h
+++ Engine/include/Error.h
@@ -15,17 +15,17 @@ using std::endl;
 #define DEBUG 0
 #define CONVERSAO_GRAUS_RADIANOS 57.324840764
 
-#define Error(msg)std::cerr<<"[ERROR] "<<__FILE__<<" | "<<__func__<<":"<<__LINE__<<"\t\t"<<msg<< "\n";exit(1);
-#define ASSERT(exp)if(!(exp)){std::cerr<<"[ERROR] "<<__FILE__<<" | "<<__func__<<":"<<__LINE__<<"\t\tAssertion Failed."<< "\n";exit(1);}
-#define SDL_ASSERT(exp)if(!(exp)){std::cerr<<"[ERROR] "<<__FILE__<<" | "<<__func__<<":"<<__LINE__<<"\t\tAssertion Failed:" << SDL_GetError()<< "\n";exit(1);}
+#define Error(msg){std::cerr<<"[ERROR] "<<__FILE__<<" | "<<__func__<<":"<<__LINE__<<"\t\t"<<msg<< "\n";exit(1);}
+#define ASSERT(exp){if(!(exp)){std::cerr<<"[ERROR] "<<__FILE__<<" | "<<__func__<<":"<<__LINE__<<"\t\tAssertion Failed."<< "\n";exit(1);}}
+#define SDL_ASSERT(exp){if(!(exp)){std::cerr<<"[ERROR] "<<__FILE__<<" | "<<__func__<<":"<<__LINE__<<"\t\tAssertion Failed:" << SDL_GetError()<< "\n";exit(1);}}
 
 #define WHERE __FILE__<<" | "<<__func__<<":"<<__LINE__
 
-#define CHECK_SDL_ERROR std::cerr<<"[ERROR] "<<__FILE__<<" | "<<__func__<<":"<<__LINE__<<"\t\t"<<SDL_GetError()<< "\n"
+#define CHECK_SDL_ERROR {std::cerr<<"[ERROR] "<<__FILE__<<" | "<<__func__<<":"<<__LINE__<<"\t\t"<<SDL_GetError()<< "\n"}
 
-#define REPORT_I_WAS_HERE if(DEBUG){std::cout <<"[DEBUG] I was here!\t"<<__FILE__<<" | "<<__func__<<":"<<__LINE__<<"\n";}
+#define REPORT_I_WAS_HERE {if(DEBUG){std::cout <<"[DEBUG] I was here!\t"<<__FILE__<<" | "<<__func__<<":"<<__LINE__<<"\n";}}
 
-#define TEMP_REPORT_I_WAS_HERE if(1){std::cout <<"[DEBUG] I was here!\t"<<__FILE__<<" | "<<__func__<<":"<<__LINE__<<"\n";}
+#define TEMP_REPORT_I_WAS_HERE {if(1){std::cout <<"[DEBUG] I was here!\t"<<__FILE__<<" | "<<__func__<<":"<<__LINE__<<"\n";}}
 
 //void Error(char const * errMsg);
 
