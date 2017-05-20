e8388f9f3d067fef02cdcd550b46f55c3d94b571
marcelo <marcelobbff@gmail.com>
2017-05-20 20:26:09 -0300

    merge draganddrop

--- Engine/src/DragAndDrop.cpp
+++ Engine/src/DragAndDrop.cpp
@@ -11,26 +11,19 @@ void DragAndDrop::Update(GameObject &associated)
 {
 	InputManager &inputManager= InputManager::GetInstance();
 	
-<<<<<<< HEAD
-	if((false == isDragging) )
-	{
-		//\todo ver se o zoom interfere aqui
-		Vec2 worldMousePos= inputManager.GetMousePos()+ Camera::pos;
-		//std::cout << WHERE << "associated.GetWorldRenderedRect()= {" << associated.GetWorldRenderedRect().x, associated.GetWorldRenderedRect().y;
-=======
-/*	if(inputManager.MousePress(RIGHT_MOUSE_BUTTON) && (false == isDragging) )
+	if(inputManager.MousePress(RIGHT_MOUSE_BUTTON) && (false == isDragging) )
 	{
 		//\todo ver se o zoom interfere aqui
 		Vec2 worldMousePos= inputManager.GetMousePos()+ Camera::pos;
 //		std::cout << WHERE << "associated.GetWorldRenderedRect()= {" << associated.GetWorldRenderedRect().x << ", " << associated.GetWorldRenderedRect().y << ", " << associated.GetWorldRenderedRect().w << ", " << associated.GetWorldRenderedRect().h << "}\n";
->>>>>>> 2aac11ffd701451ed408a8874746ee23f760feca
+
 		if(worldMousePos.IsInRect(associated.GetWorldRenderedRect()))
 		{
 			TEMP_REPORT_I_WAS_HERE;
 			isDragging= true;
 		}
 	}
-*/	if(isDragging && inputManager.IsMouseDown(RIGHT_MOUSE_BUTTON))
+	if(isDragging && inputManager.IsMouseDown(RIGHT_MOUSE_BUTTON))
 	{
 		Vec2 mousePos= inputManager.GetMousePos();
 		associated.box= mousePos+Camera::pos-Vec2(associated.box.w/2, associated.box.h/2);
