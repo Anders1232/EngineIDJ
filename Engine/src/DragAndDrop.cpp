4e53bff997a5e370ffebcdd1fa7c79097c933477
Francisco Anderson Bezerra Rodrigues <anders1232@mail.com>
2017-05-24 14:16:05 -0300

    Comentado trecho de código no DragAndDrop::Update relativo ao redrag

--- Engine/src/DragAndDrop.cpp
+++ Engine/src/DragAndDrop.cpp
@@ -10,16 +10,18 @@ DragAndDrop::DragAndDrop(TileMap *map, bool dragging, bool dragOnActionHold)
 void DragAndDrop::Update(GameObject &associated){
 	InputManager &inputManager= InputManager::GetInstance();
 	
-	if(inputManager.MousePress(RIGHT_MOUSE_BUTTON) && (false == isDragging) ){
+/*	if(inputManager.MousePress(RIGHT_MOUSE_BUTTON) && (false == isDragging) ){
 		//\todo ver se o zoom interfere aqui
 		Vec2 worldMousePos= inputManager.GetMousePos()+ Camera::pos;
 		REPORT_DEBUG("associated.GetWorldRenderedRect()= {" << associated.GetWorldRenderedRect().x << ", " << associated.GetWorldRenderedRect().y << ", " << associated.GetWorldRenderedRect().w << ", " << associated.GetWorldRenderedRect().h << "}");
 		if(worldMousePos.IsInRect(associated.GetWorldRenderedRect())){
 			TEMP_REPORT_I_WAS_HERE;
 			isDragging= true;
+			tileMap->GetTileMousePos(associated.box.Center(), true, 0);
+//			tileMap->
 		}
 	}
-	if(isDragging && inputManager.IsMouseDown(RIGHT_MOUSE_BUTTON)){
+*/	if(isDragging && inputManager.IsMouseDown(RIGHT_MOUSE_BUTTON)){
 		Vec2 mousePos= inputManager.GetMousePos();
 		associated.box= mousePos+Camera::pos-Vec2(associated.box.w/2, associated.box.h/2);
 	}
