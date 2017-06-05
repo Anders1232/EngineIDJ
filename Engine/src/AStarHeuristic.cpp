e9c399443301e0a8f701dbd92772b7b715fd8ca1
Francisco Anderson Bezerra Rodrigues <anders1232@mail.com>
2017-06-04 21:09:25 -0300

    Implementado heurística Manhattan distance.

--- Engine/src/AStarHeuristic.cpp
+++
@@ -1,3 +0,0 @@
-#include "AStarHeuristic.h"
-
-int AStarHeuristic::operator()(int originTile,int destTile ,TileMap &tilemap){}
---
+++ Engine/src/ManhattanDistance.cpp
@@ -0,0 +1,18 @@
+#include "ManhattanDistance.h"
+
+ManhattanDistance::ManhattanDistance(){
+}
+
+int operator()(int originTile,int destTile ,TileMap &tilemap){
+	int mapWidth= tilemap.GetWidth();
+	int distanceX= (destTile % mapWidth) - (originTile % mapWidth);
+	int distanceY= (originTile / mapWidth) - (originTile / mapWidth);
+	if(0 > distanceX){
+		distanceX= -distanceX;
+	}
+	if(0 > distanceY){
+		distanceY= -distanceY;
+	}
+	return distanceX+distanceY;
+}
+
