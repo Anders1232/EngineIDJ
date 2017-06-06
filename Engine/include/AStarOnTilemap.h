e6941e05bf604123c73d2e2d1caeb70b826bd427
VitorSDDF <vitor.sd@hotmail.com>
2017-06-06 02:20:30 -0300

    A* com erros de compilação

--- Engine/include/AStarOnTilemap.h
+++ Engine/include/AStarOnTilemap.h
@@ -2,12 +2,44 @@
 #include "TileMap.h"
 #include <list>
 
+template<typename T>
+
 class AStarOnTilemap{
 
+	public:
 
-	AStarOnTilemap();
+		AStarOnTilemap(int originTile,int destTile ,TileMap &tilemap,Heuristic heuristic);
+		std::list<int> FindPath();
 
-	std::list<int> operator()(int originTile,int destTile ,TileMap &tilemap, Heuristic heuristic);
+	private:
+
+		int originTile,destTile;
+		TileMap &tilemap;
+		Heuristic heuristic;
+
+		class my_priority_queue : public std::priority_queue<T, std::vector<T>>{
+  			
+  			public:
+
+      			bool remove(const T& value){
+
+        			auto it = std::find(this->c.begin(), this->c.end(), value);
+
+        			if (it != this->c.end()) {
+
+            			this->c.erase(it);
+            			std::make_heap(this->c.begin(), this->c.end(), this->comp);
+            			return true;
+
+       				}
+       				else {
+
+        				return false;
+       				}
+ 				}
+		};
 
 
 };
+
+
