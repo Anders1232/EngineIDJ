56f1aeaff5f954e7958b6c35e4c1b0a2dfebd842
Francisco Anderson Bezerra Rodrigues <anders1232@mail.com>
2017-06-06 22:04:00 -0300

    Corrigido alguns erros de compilação

--- Engine/include/AStarHeuristic.h
+++ Engine/include/AStarHeuristic.h
@@ -1,6 +1,7 @@
 #include "TileMap.h"
 
 class AStarHeuristic{
-	virtual AStarHeuristic(){};
-	virtual int operator()(int originTile,int destTile ,TileMap &tilemap)=0;
+//	virtual AStarHeuristic(){};
+	public:
+		virtual int operator()(int originTile,int destTile ,TileMap &tilemap)=0;
 };
--- Engine/src/AStartOnTilemap.cpp
+++ Engine/src/AStartOnTilemap.cpp
@@ -1,6 +1,6 @@
 #include "AStarOnTilemap.h"
 
-std::list<int> AStarOnTilemap::operator()(int originTile,int destTile ,TileMap &tilemap,Heuristic heuristic){
+std::list<int> AStarOnTilemap::operator()(int originTile,int destTile ,TileMap &tilemap,AStarHeuristic heuristic){
 
 	/*//lista  de caminhos <destino,<anterior,custo>>);
 	std::list<std::pair<unsigned int ,std::pair<unsigned int,double> > > paths;
