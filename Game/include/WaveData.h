3b5def1611513a6896e81af5a4caacf691b6f843
Francisco Anderson Bezerra Rodrigues <anders1232@mail.com>
2017-06-08 23:15:43 -0300

    Adicionado WaveData

---
+++ Game/include/WaveData.h
@@ -0,0 +1,41 @@
+#ifndef WAVEDATA_H
+#define WAVEDATA_H
+
+#include <string>
+#include <vector>
+#include "Error.h"
+
+struct EnemyData{
+		//todo: ver coo colocar cores e transparência aqui
+	public:
+		std::string name;
+		int enemyType;
+		float scaleX;
+		float sacleY;
+		std::string bodySpName;
+		std::string headSpName;
+		std::string pantsSpName;
+}
+
+struct EnemySpawnData{
+	public:
+		int enemyIndex;
+		uint quant;
+		uint baseHP;
+		uint endPoint;
+}
+
+struct SpawnPointData{
+	public:
+		std::vector<EnemySpawnData> enemySpawnData;
+}
+
+struct WaveData{
+	public:
+		WaveData();
+		
+		std::vector<SpawnPointData> spawnPointsData;
+		
+};
+
+#endif // WAVEDATA_H
