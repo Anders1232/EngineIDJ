1dbcdbc016970395fc688e6b33fcb4ec1f7bd654
Francisco Anderson Bezerra Rodrigues <anders1232@mail.com>
2017-06-18 19:26:58 -0300

    Resolvido parte do conflito

--- Engine/src/DragAndDrop.cpp
+++ Engine/src/DragAndDrop.cpp
@@ -10,21 +10,16 @@ DragAndDrop::DragAndDrop(TileMap *map,Vec2 associatedInitialPos, bool dragOnActi
 
 void DragAndDrop::Update(GameObject &associated, float dt) {
 	InputManager &inputManager= InputManager::GetInstance();
-<<<<<<< HEAD
 	if(inputManager.IsMouseDown(RIGHT_MOUSE_BUTTON)){
 		Vec2 mousePos= inputManager.GetMousePos()*(1/Camera::GetZoom());
 		associated.box= mousePos+Camera::pos-Vec2(associated.box.w/2, associated.box.h/2);
 	} 
+<<<<<<< HEAD
 	else if(inputManager.MouseRelease(RIGHT_MOUSE_BUTTON)){
 		tileMap->InsertGO(&associated,associatedInitialPos);
 		associated.RemoveComponent(DRAG_AND_DROP);
 =======
-	
-
-	if(isDragging && inputManager.IsMouseDown(RIGHT_MOUSE_BUTTON)) {
-		Vec2 mousePos = Camera::ScreenToWorld(inputManager.GetMousePos());
-		associated.box = mousePos-Vec2(associated.box.w/2, associated.box.h/2);
-	} else if(inputManager.MouseRelease(RIGHT_MOUSE_BUTTON) && isDragging) {
+	else if(inputManager.MouseRelease(RIGHT_MOUSE_BUTTON) && isDragging) {
 		tileMap->InsertGO(&associated);
 		isDragging = false;
 >>>>>>> cc3bfc49d98b75ba776e94e4a99e2a7358d33229
