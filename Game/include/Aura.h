75b41e33fa0ee47ad2ef82906d47b73d04e1f589
Francisco Anderson Bezerra Rodrigues <anders1232@mail.com>
2017-07-06 20:34:13 -0300

    Maior parte da aura está implementada, falta testar

--- Game/include/Aura.h
+++ Game/include/Aura.h
@@ -1,10 +1,28 @@
 #ifndef AURA_H
 #define AURA_H
 
-class Aura
+#include "Enemy.h"
+#include "Component.h"
+#include "Timer.h"
+#include "NearestGOFinder.h"
+
+class Aura: public Component
 {
 	public:
-		Aura();
+		Aura(GameObject& associated, Enemy::Event auraType, float auraRange, float timeBetweetNotifications, NearestGOFinder &finder);
+		~Aura(void);
+		void Update(float dt);
+		void Render(void);
+		bool Is(ComponentType type);
+	private:
+		GameObject &associated;
+		Enemy::Event auraType;
+		float auraRange;
+		Sprite sp;
+		Sprite sp2;
+		float timeBetweetNotifications;
+		Timer notificationTimer;
+		NearestGOFinder &finder;
 };
 
 #endif // AURA_H
--- Game/src/Aura.cpp
+++ Game/src/Aura.cpp
@@ -1,5 +1,44 @@
 #include "Aura.h"
 
-Aura::Aura()
-{
+Aura::Aura(GameObject &associated,
+			Enemy::Event auraType,
+			float auraRange,
+			float timeBetweetNotifications,
+			NearestGOFinder &finder)
+	:associated(associated),
+		auraType(auraType),
+		auraRange(auraRange),
+		timeBetweetNotifications(timeBetweetNotifications),
+		finder(finder){
+	if(Enemy::Event::SMOKE == auraType){
+		
+	}
+	else if(Enemy::Event::STUN == auraType){
+		
+	}
+	else if(Enemy::Event::HEALER == auraType){
+		
+	}
 }
+
+void Aura::Update(float dt){
+	notificationTimer.Update(dt);
+	if(notificationTimer.Get() > timeBetweetNotifications){
+		notificationTimer.Restart();
+		vector<GameObject *> *enemiesInRange= finder.FindNearestGOs(associated.box.Center(), "Enemies",, range);
+		for(int i=0; i< enemiesInRange; i++){
+			( (Enemy*)(enemiesInRange[i]) )->NotifyEvent(auraType);
+		}
+		delete enemiesInRange;
+	}
+}
+
+void Aura::Render(void){
+	Vec2 startPoint= associated.box;
+	startPoint= startPoint - Vec2(sp.GetWidth()/2, sp.GetHeight()/2);
+	sp.Render(Rect(startPoint.x, startPoint.y, sp.GetWidth(), sp.GetHeight()));
+	startPoint= associated.box;
+	startPoint= startPoint - Vec2(sp2.GetWidth()/2, sp2.GetHeight()/2);
+	sp2.Render(Rect(startPoint.x, startPoint.y, sp2.GetWidth(), sp2.GetHeight()));
+}
+
