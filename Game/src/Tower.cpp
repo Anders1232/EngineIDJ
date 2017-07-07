0e7e823f3b6f583d764bfd13dee50e69cf660476
Francisco Anderson Bezerra Rodrigues <anders1232@mail.com>
2017-07-07 05:20:27 -0300

    Colocado de volta as torres-aura

--- Game/src/Tower.cpp
+++ Game/src/Tower.cpp
@@ -26,63 +26,20 @@ Tower::Tower(TowerType type, Vec2 pos, Vec2 tileSize, int hp)
 	box.h = sp.GetHeight();
 	StageState& stageState= (StageState&)Game::GetInstance().GetCurrentState();
 
-#ifdef SORTEAR_TORRES
-	int sorteio = rand()%4;
-	if(0 == sorteio){
-		switch(type){
-			case TowerType::SMOKE:
-				AddComponent(new Shooter(*this, (NearestGOFinder&)stageState, "Enemy", 5000, 2.0, Shooter::TargetPolicy::ALWAYS_NEAREST, true, 500, 5000, "img/spritesheets/fumaça_spritesheet.png", 8, 1));
-				break;
-			case TowerType::ANTIBOMB:
-				AddComponent(new Shooter(*this, (NearestGOFinder&)stageState, "Enemy", 5000, 2.0, Shooter::TargetPolicy::ALWAYS_NEAREST, true, 500, 5000, "img/spritesheets/anti-bomba_ativ_spritesheet.png", 11, 1));
-				break;
-			case TowerType::STUN:
-				AddComponent(new Shooter(*this, (NearestGOFinder&)stageState, "Enemy", 5000, 2.0, Shooter::TargetPolicy::ALWAYS_NEAREST, true, 500, 5000, "img/spritesheets/stun_spritesheet.png", 8, 1));
-				break;
-			case TowerType::SHOCK:
-				AddComponent(new Shooter(*this, (NearestGOFinder&)stageState, "Enemy", 5000, 2.0, Shooter::TargetPolicy::ALWAYS_NEAREST, true, 500, 5000, "img/spritesheets/bullet_choquelvl1.png", 4, 1));
-				break;
-		}
-	}
-	else if (1 == sorteio){
-		AddComponent(new Aura(*this, Enemy::Event::SMOKE, 300, 7.0, (NearestGOFinder&)stageState, "Enemy"));
-	}
-	else if (2 == sorteio){
-		AddComponent(new Aura(*this, Enemy::Event::SMOKE, 300, 1.0, (NearestGOFinder&)stageState, "Enemy"));
-	}
-	else{
-		AddComponent(new Shooter(*this, (NearestGOFinder&)stageState, "BOMB", 1000, 3.0, Shooter::TargetPolicy::ALWAYS_NEAREST, true, 500, 3000, "img/SpriteSheets/anti-bomba_idle.png", 5, 2.0));
-		switch(type){
-			case TowerType::SMOKE:
-				AddComponent(new Shooter(*this, (NearestGOFinder&)stageState, "BOMB", 1000, 3.0, Shooter::TargetPolicy::ALWAYS_NEAREST, true, 500, 3000, "img/spritesheets/fumaça_spritesheet.png", 8, 1));
-				break;
-			case TowerType::ANTIBOMB:
-				AddComponent(new Shooter(*this, (NearestGOFinder&)stageState, "BOMB", 1000, 3.0, Shooter::TargetPolicy::ALWAYS_NEAREST, true, 500, 3000, "img/spritesheets/anti-bomba_ativ_spritesheet.png", 11, 1));
-				break;
-			case TowerType::STUN:
-				AddComponent(new Shooter(*this, (NearestGOFinder&)stageState, "BOMB", 1000, 3.0, Shooter::TargetPolicy::ALWAYS_NEAREST, true, 500, 3000, "img/spritesheets/stun_spritesheet.png", 8, 1));
-				break;
-			case TowerType::SHOCK:
-				AddComponent(new Shooter(*this, (NearestGOFinder&)stageState, "BOMB", 1000, 3.0, Shooter::TargetPolicy::ALWAYS_NEAREST, true, 500, 3000, "img/spritesheets/bullet_choquelvl1.png", 4, 1));
-				break;
-		}
-	}
-#else
 	switch(type){
 		case TowerType::SMOKE:
-			AddComponent(new Shooter(*this, (NearestGOFinder&)stageState, "Enemy", 5000, 2.0, Shooter::TargetPolicy::ALWAYS_NEAREST, true, 500, 5000, "img/spritesheets/fumaça_spritesheet.png", 8, 1));
+			AddComponent(new Aura(*this, Enemy::Event::SMOKE, 800, 7.0, (NearestGOFinder&)stageState, "Enemy"));
 			break;
 		case TowerType::ANTIBOMB:
 			AddComponent(new Shooter(*this, (NearestGOFinder&)stageState, "Enemy", 5000, 2.0, Shooter::TargetPolicy::ALWAYS_NEAREST, true, 500, 5000, "img/spritesheets/anti-bomba_ativ_spritesheet.png", 11, 1));
 			break;
 		case TowerType::STUN:
-			AddComponent(new Shooter(*this, (NearestGOFinder&)stageState, "Enemy", 5000, 2.0, Shooter::TargetPolicy::ALWAYS_NEAREST, true, 500, 5000, "img/spritesheets/stun_spritesheet.png", 8, 1));
+			AddComponent(new Aura(*this, Enemy::Event::STUN, 800, 7.0, (NearestGOFinder&)stageState, "Enemy"));
 			break;
 		case TowerType::SHOCK:
 			AddComponent(new Shooter(*this, (NearestGOFinder&)stageState, "Enemy", 5000, 2.0, Shooter::TargetPolicy::ALWAYS_NEAREST, true, 500, 5000, "img/spritesheets/bullet_choquelvl1.png", 4, 1));
 			break;
 	}
-#endif
 
 	hitpoints = new HitPoints(hp,*this);
 	components.push_back(hitpoints);
